4281021c19f64f5df79a9d7a6ffea483343331fa
Jing Xue <97304545+jingxue8303@users.noreply.github.com>
2022-09-15 15:31:22 +0000

    Completed binsearch.py

--- py/binsearch.py
+++ py/binsearch.py
@@ -2,22 +2,33 @@
 # Jing Xue
 # CSCI 77800 Fall 2022
 # collaborators: 
-# consulted: 
+# consulted: online resource "GeeksforGeeks"
 
-def binsearchOne(a, t):
-  low = 0
-  high = len(a) - 1
-  mid = int((low + high) / 2)
-  while (high > low):
+def binary_search(a, low, high, t):
+  if high >= low:
+    mid = (high + low) // 2  #round down to integer
     if a[mid] == t:
-     print (mid)
-    elif low >= high :
-      print(-1)
-    elif (a[mid] < t):
-      low = mid +1
-    else : 
-      high = mid -1
-    mid = int((low + high) / 2)
+      return mid
+    elif a[mid] > t:
+      return binary_search (a, low, mid-1, t)
+    else :
+      return binary_search (a, mid +1, high, t)
+  else : 
+    return -1
+    
+  #low = 0
+  #high = len(a) - 1
+  #mid = int((low + high) / 2)
+  #while (high > low):
+  #  if a[mid] == t:
+  #   print (mid)
+  #  elif low >= high :
+  #    print(-1)
+  #  elif (a[mid] < t):
+  #    low = mid +1
+  #  else : 
+  #    high = mid -1
+  #  mid = int((low + high) / 2)
 
 #def binsearchRecursive (a, value, loIndex, hiIndex):
   #if hiIndex >= loIndex :
@@ -30,7 +41,16 @@ def binsearchOne(a, t):
 #  print(-1)
 
 
-binsearchOne([1,2,3], 4)
+# Test with given array
+a = [1, 3, 5, 7, 9, 20]
+t = 20
+
+result = binary_search (a, 0, len(a)-1, t)
+
+if result != -1:
+  print ("Element is present at index", str (result))
+else:
+  print ("Element not found in array")
 
 
       
