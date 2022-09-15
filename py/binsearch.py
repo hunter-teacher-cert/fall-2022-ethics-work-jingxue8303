0f8f7aca1f864c66b4c782261d60353786f77b90
Jing Xue <97304545+jingxue8303@users.noreply.github.com>
2022-09-15 00:46:05 +0000

    started binsearch.py

--- py/binsearch.py
+++ py/binsearch.py
@@ -3,3 +3,35 @@
 # CSCI 77800 Fall 2022
 # collaborators: 
 # consulted: 
+
+def binsearchOne(a, t):
+  low = 0
+  high = len(a) - 1
+  mid = int((low + high) / 2)
+  while (high > low):
+    if a[mid] == t:
+     print (mid)
+    elif low >= high :
+      print(-1)
+    elif (a[mid] < t):
+      low = mid +1
+    else : 
+      high = mid -1
+    mid = int((low + high) / 2)
+
+#def binsearchRecursive (a, value, loIndex, hiIndex):
+  #if hiIndex >= loIndex :
+  #  mid = (hiIndex + loIndex) / 2
+  #  if a[mid] == value:
+  #    print(mid)
+  #  if a[mid] < value:
+ #     binsearchRecursive(a, value, mid+1, hiIndex)
+  #  binsearchRecursive(a, value, loIndex, mid-1)
+#  print(-1)
+
+
+binsearchOne([1,2,3], 4)
+
+
+      
+  
--- py/gol.py
+++ py/gol.py
@@ -3,3 +3,10 @@
 # CSCI 77800 Fall 2022
 # collaborators: 
 # consulted: 
+
+#def printCol(c):
+
+
+
+
+#createNewBoard (5,5)
