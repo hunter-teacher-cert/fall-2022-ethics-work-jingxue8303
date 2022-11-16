97405ec099c413dbf8ba5a240293188d09c4158f
Jing Xue <97304545+jingxue8303@users.noreply.github.com>
2022-11-16 12:51:51 +0000

    fixed some bugs for seating2.py

--- week09_seating/plane_seating2.py
+++ week09_seating/plane_seating2.py
@@ -1,13 +1,13 @@
-#Collaborators:Elizabeth                                       Jing
+#Collaborators:Elizabeth,                    ,                   , Jing
 #Resource and Reference: https://github.com/ryanhoang15/Python/blob/master/9.22%20Lab%20Ch%209:%20Concert%20seating%20chart(2D).py
 
 
-price_chart = [[40, 40, 40, 0, 40, 40, 40, 40, 40, 0, 40, 40, 40],
-               [20, 20, 30, 0, 30, 20, 20, 20, 30, 0, 30, 20, 20],
-               [20, 20, 30, 0, 30, 20, 20, 20, 30, 0, 30, 20, 20],
-               [20, 20, 30, 0, 30, 20, 20, 20, 30, 0, 30, 20, 20],
-               [20, 20, 30, 0, 30, 20, 20, 20, 30, 0, 30, 20, 20]]
-# This is a small scale of airplane seating. The numbers represent the price. 40s are the front rows(discounts can be offered to certain groups by airlines); 30s are by the corridor(the initial 0s); 1st colume and last colume are by the window.
+price_chart = [[4, 4, 4, 8, 4, 4, 4, 4, 4, 8, 4, 4, 4],
+               [2, 2, 3, 8, 3, 2, 2, 2, 3, 8, 3, 2, 2],
+               [2, 2, 3, 8, 3, 2, 2, 2, 3, 8, 3, 2, 2],
+               [2, 2, 3, 8, 3, 2, 2, 2, 3, 8, 3, 2, 2],
+               [2, 2, 3, 8, 3, 2, 2, 2, 3, 8, 3, 2, 2]]
+# This is a small scale of airplane seating. The numbers represent the price. We picked single digit because double-digit numbers don't line-up. Number 4 are the front rows seats (discounts may be offered to certain groups by airlines); Number 3 are the seats by the corridor(the 8s); 1st column and last column are seats by the window.
 
 print("Here are the available seats:\n")
 
@@ -22,34 +22,34 @@ while user_input != "q":
     if user_input == "s":
         row = int(input("Enter the row:\n "))-1
         col = int(input("Enter the column:\n "))-1
-        if price_chart[col][row] != 0:
-            print("Sold, for $", price_chart[col][row], "!\n")
-            price_chart[col][row] = 0
+        if price_chart[row][col] != 0:
+            print("Sold, for $", price_chart[row][col], "!\n")
+            price_chart[row][col] = 0
         else:
             print("Sorry, that seat is not available.\n")
 
-    elif user_input == "p": #picking a seat by price
-        money = int(input("How much do you want to spend?(20,30,40)\n "))
+    elif user_input == "p": #choose a seat by price
+        money = int(input("How much do you want to spend?(2,3,4)\n "))
         found = False
-        for x, col in enumerate(price_chart):
-            for y, row in enumerate(col):
+        for y, col in enumerate(price_chart):
+            for x, row in enumerate(col):
                 if row == money and not found:
                     found = True
-                    print("You can have seat", x+1, y+1) #give the first available seat number for the user to type in. We believe this may privide consective seats if needed.)
-                    break;
+                    print("You can have seat (row, column): ", y+1, x+1) #give the first available seat number for the user to type in. We believe this may privide consective seats if needed.)
+                    print()
         if not found:
             print("Sorry, no tickets available at that price.\n")
     else:
         print("That wasn't a valid option.\n")
       
-    print("Here are the available seats:\n")
+    #print("Here are the available seats:\n")
+    #for x, row in enumerate(price_chart):
+      #for col in price_chart[x]:
+        #print(col, end=" ")
+        #print()
+      # show the seating everytime a purchase is finished
+    user_input = input("Pick a (S)eat, a (P)rice or (Q)uit:\n ").lower()
 
-  for x, row in enumerate(price_chart):
-    for col in price_chart[x]:
-      print(col, end=" ")
-      print()
-      
-  user_input = input("Pick a (S)eat, a (P)rice or (Q)uit:\n ").lower()
 for x, row in enumerate(price_chart):
     for col in price_chart[x]:
         print(col, end=" ")
